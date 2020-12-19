9f65e6cf2ef807ea5e1972a020056a4a4615d049
Aadarsh MK <75124918+AADARSH-MADAN-KOLLAN@users.noreply.github.com>
2020-12-19 11:55:27 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -6,20 +6,20 @@ class Paper {
           'friction':0.5,
           'density':1.2
       }
-      this.body = Bodies.circle(x, y, r, options);
       this.radius = r;
+      this.x = x;
+      this.y = y;
+      this.body = Bodies.circle(x, y, r, options);
       
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
-      var angle = this.body.angle;
       push();
       translate(pos.x, pos.y);
-      rotate(angle);
-      ellipseMode(CENTER);
       fill("white");
-      ellipse(0, 0, this.radius);
+      ellipseMode(CENTER)
+      ellipse(0, 0, this.radius, this.radius);
       pop();
     }
   };
